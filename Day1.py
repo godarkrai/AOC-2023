4febe33df34d353c70282125d90323d02a44ed7d
Shashank Atreya <satreya@liquipedia.net>
2023-12-01 17:46:47 +0530

    optimize

--- Day1.py
+++ Day1.py
@@ -1,22 +1,32 @@
+from collections import defaultdict
+
 with open( 'Day1Input.txt' ) as f:
     input = f.read().splitlines()
 
 def part1():
+    # Two Pointer
     total = 0
     for line in input:
-        numbers = []
-        for ch in line:
-            if ch.isnumeric():
-                numbers.append(ch)
-        if len(numbers) == 1:
-            curTotal = numbers[0] * 2
-        else:
-            curTotal = numbers[0] + numbers[-1]
-        total += int(curTotal)
+        left = 0
+        right = len(line) - 1
+        firstNumber = ''
+        lastNumber = ''
+        while left <= right:
+            if line[left].isnumeric() and firstNumber == '':
+                firstNumber = line[left]
+            if line[right].isnumeric() and lastNumber == '':
+                lastNumber = line[right]
+            if firstNumber != '' and lastNumber != '':
+                break
+            if firstNumber == '':
+                left += 1
+            if lastNumber == '':
+                right -= 1
+        total += int(firstNumber+lastNumber)
     return total
 
 def part2():
-    res = []
+    total = 0
     validNumberWords = {
         'one': '1', 'two': '2', 'three': '3', 'four': '4',
         'five': '5', 'six': '6', 'seven': '7',
@@ -24,42 +34,44 @@ def part2():
     }
     validNumbers = [ '1', '2', '3', '4', '5', '6', '7', '8', '9' ]
     for line in input:
-        minWordIndex = float( 'inf' )
-        maxWordIndex = float( '-inf' )
-        minNumberIndex = float( 'inf' )
-        maxNumberIndex = float( '-inf' )
-        minWord = ''
-        minNumber = ''
-        maxWord = ''
-        maxNumber = ''
+        wordsAt = defaultdict(tuple)
+        numbersAt = defaultdict(tuple)
         for validWord in validNumberWords:
             minWordAt = line.find( validWord )
             maxWordAt = line.rfind( validWord )
-            if minWordAt != -1 and minWordAt < minWordIndex:
-                minWordIndex = minWordAt
-                minWord = validWord
-            if maxWordAt != -1 and maxWordAt > maxWordIndex:
-                maxWordIndex = maxWordAt
-                maxWord = validWord
+            wordsAt[ validWord ] = (
+                minWordAt,
+                maxWordAt,
+                validNumberWords[ validWord ]
+            )
         for validNumber in validNumbers:
             minNumberAt = line.find( validNumber )
             maxNumberAt = line.rfind( validNumber )
-            if minNumberAt != -1 and minNumberAt < minNumberIndex:
-                minNumberIndex = minNumberAt
-                minNumber = validNumber
-            if maxNumberAt > maxNumberIndex:
-                maxNumberIndex = maxNumberAt
-                maxNumber = validNumber
-        if minWordIndex < minNumberIndex:
-            firstLetter = validNumberWords[ minWord ]
-        else:
-            firstLetter = minNumber
-        
-        if maxWordIndex > maxNumberIndex:
-            lastLetter = validNumberWords[ maxWord ]
-        else:
-            lastLetter = maxNumber
-        res.append(firstLetter+lastLetter)
-    return sum( list( map(int, res) ) )
+            numbersAt[ validNumber ] = (
+                minNumberAt,
+                maxNumberAt,
+                validNumber
+            )
+        # Get the minimum and maximum index from words
+        minIndex = float( 'inf' )
+        maxIndex = float( '-inf' )
+        firstNumber = ''
+        lastNumber = ''
+        for (minIdx, maxIdx, number) in wordsAt.values():
+            if minIdx != -1 and minIdx < minIndex:
+                minIndex = minIdx
+                firstNumber = number
+            if maxIdx != -1 and maxIdx > maxIndex:
+                maxIndex = maxIdx
+                lastNumber = number
+        for (minIdx, maxIdx, number) in numbersAt.values():
+            if minIdx != -1 and minIdx < minIndex:
+                minIndex = minIdx
+                firstNumber = number
+            if maxIdx != -1 and maxIdx > maxIndex:
+                maxIndex = maxIdx
+                lastNumber = number
+        total += int(firstNumber+lastNumber)
+    return total
 print( "Part 1:", part1() ) # 55130
 print( "Part 2:", part2() ) # 54985
